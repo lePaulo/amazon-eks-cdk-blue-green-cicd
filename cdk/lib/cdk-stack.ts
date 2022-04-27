d16306618b2dc51295e34609772988818e2e878b
lePaulo <lePaulo@users.noreply.github.com>
2022-04-27 23:41:13 +0200

    Update with an ECR-based pipeline + an Aurora cluster and a static S3/CloudFront website (#1)
    * Update package.json
    
    * Update cdk-stack.ts

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,11 +4,16 @@ import ec2 = require('@aws-cdk/aws-ec2');
 import ecr = require('@aws-cdk/aws-ecr');
 import eks = require('@aws-cdk/aws-eks');
 import iam = require('@aws-cdk/aws-iam');
+import rds = require('@aws-cdk/aws-rds');
+import s3 = require('@aws-cdk/aws-s3');
+import s3deploy = require('@aws-cdk/aws-s3-deployment');
+import cloudfront = require('@aws-cdk/aws-cloudfront');
 import codebuild = require('@aws-cdk/aws-codebuild');
 import codecommit = require('@aws-cdk/aws-codecommit');
 import targets = require('@aws-cdk/aws-events-targets');
 import codepipeline = require('@aws-cdk/aws-codepipeline');
 import codepipeline_actions = require('@aws-cdk/aws-codepipeline-actions');
+import { RemovalPolicy } from '@aws-cdk/core';
 
 
 
@@ -54,7 +59,29 @@ export class CdkStackALBEksBg extends cdk.Stack {
       repositoryName: `${this.stackName}-repo`
     });
 
+    // Create RDS instance with secret manager credentials
+    
+    const auroracluster = new rds.DatabaseCluster(this, 'Database', {
+      engine: rds.DatabaseClusterEngine.AURORA,
+      instanceProps: { vpc },
+    });
+    
+    // PIPELINE SOURCE ACTIONS
+    const sourceOutput = new codepipeline.Artifact();
+    const sourceImageOutput = new codepipeline.Artifact();
 
+    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
+      actionName: 'CodeCommit',
+      repository,
+      output: sourceOutput,
+    });
+    
+    const sourceEcrAction = new codepipeline_actions.EcrSourceAction({
+      actionName: 'ECR',
+      repository: ecrRepo,
+      imageTag: 'latest',
+      output: sourceImageOutput,
+    });
 
     // CODEBUILD - project
     const project = new codebuild.Project(this, 'MyProject', {
@@ -80,7 +107,7 @@ export class CdkStackALBEksBg extends cdk.Stack {
           pre_build: {
             commands: [
               'env',
-              'export TAG=${CODEBUILD_RESOLVED_SOURCE_VERSION}',
+              'export TAG=$(cat ${CODEBUILD_SRC_DIR_Artifact_Source_ECR}/imageDetail.json | jq -r \'.ImageTags[0]\')',
               'export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output=text)',
               '/usr/local/bin/entrypoint.sh',
               'echo Logging in to Amazon ECR',
@@ -89,9 +116,8 @@ export class CdkStackALBEksBg extends cdk.Stack {
           },
           build: {
             commands: [
-              'cd flask-docker-app',
-              `docker build -t $ECR_REPO_URI:$TAG .`,
-              'docker push $ECR_REPO_URI:$TAG'
+              'echo $ECR_REPO_URI:$TAG',
+              'docker pull $ECR_REPO_URI:$TAG'
             ]
           },
           post_build: {
@@ -166,24 +192,13 @@ export class CdkStackALBEksBg extends cdk.Stack {
       })
     })
 
-
-
-
-
     // PIPELINE
-
-    const sourceOutput = new codepipeline.Artifact();
-
-    const sourceAction = new codepipeline_actions.CodeCommitSourceAction({
-      actionName: 'CodeCommit',
-      repository,
-      output: sourceOutput,
-    });
 
     const buildAction = new codepipeline_actions.CodeBuildAction({
       actionName: 'CodeBuild',
       project: project,
       input: sourceOutput,
+      extraInputs: [sourceImageOutput],
       outputs: [new codepipeline.Artifact()], // optional
     });
 
@@ -198,14 +213,13 @@ export class CdkStackALBEksBg extends cdk.Stack {
     const manualApprovalAction = new codepipeline_actions.ManualApprovalAction({
       actionName: 'Approve',
     });
+    
 
-
-
-    new codepipeline.Pipeline(this, 'MyPipeline', {
+    const myCodePipeline = new codepipeline.Pipeline(this, 'MyPipeline', {
       stages: [
         {
           stageName: 'Source',
-          actions: [sourceAction],
+          actions: [sourceAction, sourceEcrAction],
         },
         {
           stageName: 'BuildAndDeploy',
@@ -221,19 +235,17 @@ export class CdkStackALBEksBg extends cdk.Stack {
         },
       ],
     });
-
-
-    repository.onCommit('OnCommit', {
-      target: new targets.CodeBuildProject(project)
-    });
-
+    
+    ecrRepo.onCloudTrailImagePushed('onCloudTrailImagePushed',{
+      target: new targets.CodePipeline(myCodePipeline)
+    })
+    
     ecrRepo.grantPullPush(project.role!)
     cluster.awsAuth.addMastersRole(project.role!)
     project.addToRolePolicy(new iam.PolicyStatement({
       actions: ['eks:DescribeCluster'],
       resources: [`${cluster.clusterArn}`],
     }))
-
 
     ecrRepo.grantPullPush(project2.role!)
     cluster.awsAuth.addMastersRole(project2.role!)
@@ -242,10 +254,71 @@ export class CdkStackALBEksBg extends cdk.Stack {
       resources: [`${cluster.clusterArn}`],
     }))
 
+    //Static Website part
+    
+    // Content bucket
+      
+    const cloudfrontOAI = new cloudfront.OriginAccessIdentity(this, 'cloudfront-OAI', {
+      comment: `OAI for static website`
+    });  
+    
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'error.html',
+      publicReadAccess: false,
+      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+
+      /**
+       * The default removal policy is RETAIN, which means that cdk destroy will not attempt to delete
+       * the new bucket, and it will remain in your account until manually deleted. By setting the policy to
+       * DESTROY, cdk destroy will attempt to delete the bucket, but will error if the bucket is not empty.
+       */
+      removalPolicy: RemovalPolicy.DESTROY, // NOT recommended for production code
+
+      /**
+       * For sample purposes only, if you create an S3 bucket then populate it, stack destruction fails.  This
+       * setting will enable full cleanup of the demo.
+       */
+      autoDeleteObjects: true, // NOT recommended for production code
+    });
+    // Grant access to cloudfront
+    siteBucket.addToResourcePolicy(new iam.PolicyStatement({
+      actions: ['s3:GetObject'],
+      resources: [siteBucket.arnForObjects('*')],
+      principals: [new iam.CanonicalUserPrincipal(cloudfrontOAI.cloudFrontOriginAccessIdentityS3CanonicalUserId)]
+    }));
+    
+    // CloudFront distribution
+    const distribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+      originConfigs: [
+        {
+          s3OriginSource: {
+            s3BucketSource: siteBucket,
+            originAccessIdentity: cloudfrontOAI
+          },
+          behaviors: [{
+            isDefaultBehavior: true,
+            compress: true,
+            allowedMethods: cloudfront.CloudFrontAllowedMethods.GET_HEAD_OPTIONS,
+          }],
+        }
+      ]
+    });
+    
+    // Deploy site contents to S3 bucket
+    new s3deploy.BucketDeployment(this, 'DeployWithInvalidation', {
+      sources: [s3deploy.Source.asset('./site-contents')],
+      destinationBucket: siteBucket,
+      distribution,
+      distributionPaths: ['/*'],
+    });
 
     new cdk.CfnOutput(this, 'CodeCommitRepoName', { value: `${repository.repositoryName}` })
     new cdk.CfnOutput(this, 'CodeCommitRepoArn', { value: `${repository.repositoryArn}` })
     new cdk.CfnOutput(this, 'CodeCommitCloneUrlSsh', { value: `${repository.repositoryCloneUrlSsh}` })
     new cdk.CfnOutput(this, 'CodeCommitCloneUrlHttp', { value: `${repository.repositoryCloneUrlHttp}` })
+    new cdk.CfnOutput(this, 'RDSEndpoint', { value: `${auroracluster.clusterEndpoint.hostname}` });
+    new cdk.CfnOutput(this, 'DistributionId', { value: distribution.distributionId });
+    new cdk.CfnOutput(this, 'Bucket', { value: siteBucket.bucketName });
   }
 }
